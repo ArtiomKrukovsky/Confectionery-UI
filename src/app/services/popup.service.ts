23fd00d65506ee5d69af9deee3d49c43672c9855
Artsiom Krukosuki <artiomgood1@gmail.com>
2022-11-07 14:11:17 +0300

    fix popup bugs with scaling and scrolling

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { WindowScrollingService } from './window-scrolling.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PopupService {
+  public isVisible$ = new BehaviorSubject<boolean>(false);
 
-  isVisible$ = new BehaviorSubject<boolean>(false);
+  constructor(private windowScrollingService: WindowScrollingService) { }
 
   public open() {
+    this.windowScrollingService.disable();
     this.isVisible$.next(true);
   }
 
   public close() {
+    this.windowScrollingService.enable();
     this.isVisible$.next(false);
   }
-
-  constructor() { }
-  
 }
