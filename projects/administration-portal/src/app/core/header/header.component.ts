a3527db68a5fc853cf0306cded76c33b74c17ba9
Artsiom Krukosuki <artiomgood1@gmail.com>
2023-09-06 09:30:05 +0300

    add menu burger with styles

--- projects/administration-portal/src/app/core/header/header.component.ts
+++ projects/administration-portal/src/app/core/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public isMenuOpened: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public openBurgerMenu(): void {
+    // if (this.isMenuOpened) {
+    //   this.windowScrollingSerive.enableScrolling();
+    // } else {
+    //   this.windowScrollingSerive.disableScrolling();
+    // }
+
+    this.isMenuOpened = !this.isMenuOpened;
+  }
 }
